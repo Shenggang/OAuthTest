b60de8b0f5e338bbfd4fc26db123b22535a0ba1a
Shenggang Hu <hushenggang1996@163.com>
2020-12-06 17:46:32 +0000

    save

--- oauth_test/core/video_list.py
+++ oauth_test/core/video_list.py
@@ -1,5 +1,28 @@
 
 class VideoList:
 
-    def __init__(self):
-        x = 1
+    def __init__(self,
+                 api_key):
+        self._api_key = api_key
+        self.video_list = dict()
+
+    @property
+    def api_key(self):
+        return self._api_key
+
+    @api_key.setter
+    def api_key(self, value):
+        self._api_key = value
+
+    def add_entry(self, key, arg):
+        self.video_list[key] = arg
+
+    def load_from_dict(self, d):
+        self.video_list = d
+
+    def update(self):
+        # build request
+
+        # check for length
+
+        # if length not equal, reconstruct
